642450e24e488919aff5cba780ddf3db843156ce
Cmb <71194682+29cmb@users.noreply.github.com>
2025-01-26 10:21:28 -0500

    feat(enhancement): :sparkles: Flaming enhancement and kaboomer joker

--- main.lua
+++ main.lua
@@ -9,7 +9,43 @@ SMODS.Atlas {
 }
 
 SMODS.Enhancement {
+    key = "flaming",
+    loc_txt = {
+        name = "Flaming",
+        text = {
+            "{C:mult}+#1#{} mult",
+            "Increases by {C:gold}#2#{} every time this card is played",
+            "{C:red}Has a 1/8 chance to destroy this card{}"
+        },
+    },
+    config = {mult = 5, increase = 1},
+    loc_vars = function(self, info_queue, card)
+        return {vars = {card.ability.mult, card.ability.increase}}
+    end,
+    calculate = function(self, card, context)
+        if context.cardarea == G.play and context.main_scoring then 
+            card.ability.mult = card.ability.mult + card.ability.increase
+        end
+        
+        if context.destroying_card then 
+            if pseudorandom("kaboomers_fire_destroy", 1, 8) == 1 then 
+                card_eval_status_text(card, 'extra', nil, nil, nil, {message = "Extinct!"})
 
+                for _,v in pairs(G.jokers.cards) do 
+                    if v.label == "j_kb_kaboomer" then 
+                        v.ability.x_mult = v.ability.x_mult + v.ability.increase
+                        card_eval_status_text(v, 'extra', nil, nil, nil, {message = "X" .. v.ability.x_mult})
+                    end
+                end
+
+                return {
+                    remove = true
+                }
+            else
+                card_eval_status_text(card, "extra", nil, nil, nil, {message = "Safe!"})
+            end
+        end
+    end
 }
 
 SMODS.Joker {
@@ -17,7 +53,9 @@ SMODS.Joker {
     loc_txt = {
         name = "Kaboomer",
         text = {
-            "TODO"
+            "Gains {X:mult,C:white}X#1#{} mult",
+            "for every {C:red}Fire{} card destroyed!",
+            "{C:inactive}(Currently {X:mult,C:white}X#2#{}){}"
         },
     },
     pos = { x = 0, y = 0 },
@@ -25,8 +63,8 @@ SMODS.Joker {
     soul_pos = { x = 0, y = 1 },
     rarity = 4,
     cost = 20,
-    config = {extra = { x_mult = 3 }},
-    calculate = function(self, card, context)
-
+    config = {x_mult = 10, increase = 1},
+    loc_vars = function(self, info_queue, card)
+        return {vars = {card.ability.increase, card.ability.x_mult}}
     end
 }
