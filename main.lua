68036dbc39e35e7ad3a55c6e95ab8204cbe156de
Cmb <71194682+29cmb@users.noreply.github.com>
2025-01-26 20:06:48 -0500

    style: :recycle: Restructure some functions and add globals to the `.vscode/settings.json` file

--- main.lua
+++ main.lua
@@ -108,7 +108,12 @@ SMODS.Consumable {
         end 
         for i = 1, #G.hand.highlighted do --unflips cards
             local percent = 0.85 + (i-0.999)/(#G.hand.highlighted-0.998)*0.3
-            G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.15,func = function() G.hand.highlighted[i]:flip();play_sound('tarot2', percent, 0.6);G.hand.highlighted[i]:juice_up(0.3, 0.3);return true end }))
+            G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.15,func = function() 
+                G.hand.highlighted[i]:flip()
+                play_sound('tarot2', percent, 0.6)
+                G.hand.highlighted[i]:juice_up(0.3, 0.3)
+                return true 
+            end}))
         end
         G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2,func = function() G.hand:unhighlight_all(); return true end }))
         delay(0.5)
